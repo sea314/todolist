3e41bfde5d46e8ca441e0a89b76902bdb20e61ca
sea314 <3141592yuki11235813@gmail.com>
2022-05-19 00:26:09 +0900

    todo出来た

---
+++ src/components/ToDoList.vue
@@ -0,0 +1,62 @@
+<script setup>
+import { ref } from "vue";
+
+const items = ref([
+  { name: "やること1", isfinished: false, limit: "2022/12" },
+  { name: "やること2", isfinished: false, limit: "2022/123213113131424" },
+]);
+const newItemName = ref("");
+const newItemLimit = ref("");
+
+const addItem = () => {
+  if (newItemName.value.length > 0) {
+    items.value.push({
+      name: newItemName.value,
+      isfinished: false,
+      limit: newItemLimit.value,
+    });
+    newItemName.value = "";
+    newItemLimit.value = "";
+  }
+};
+</script>
+
+<template>
+  <div>ToDoList</div>
+  未完了
+  <div v-for="item in items" :key="item.name">
+    <div v-if="!item.isfinished">
+      <div class="item">
+        <input v-model="item.isfinished" type="checkbox" />
+        {{ item.name }} 期限：{{ item.limit }}
+      </div>
+    </div>
+  </div>
+  完了
+  <div v-for="item in items" :key="item.name">
+    <div v-if="item.isfinished">
+      <div class="item">
+        <input v-model="item.isfinished" type="checkbox" />
+        {{ item.name }} 期限：{{ item.limit }}
+      </div>
+    </div>
+  </div>
+
+  <div>
+    <label>
+      名前
+      <input v-model="newItemName" type="text" />
+    </label>
+    <label>
+      期限
+      <input v-model="newItemLimit" type="text" />
+    </label>
+    <button @click="addItem">add</button>
+  </div>
+</template>
+
+<style>
+.over500 {
+  color: red;
+}
+</style>
