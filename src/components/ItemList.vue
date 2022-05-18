0ceda1b546a759437dd08c05b91f687c525ddc08
sea314 <3141592yuki11235813@gmail.com>
2022-05-18 23:57:39 +0900

    商品リスト

---
+++ src/components/ItemList.vue
@@ -0,0 +1,47 @@
+<script setup>
+import { ref } from "vue";
+
+const items = ref([
+  { name: "たまご", price: 100 },
+  { name: "りんご", price: 160 },
+]);
+const newItemName = ref("");
+const newItemPrice = ref(0);
+
+const addItem = () => {
+  if (newItemName.value.length > 0 && newItemPrice.value > 0) {
+    items.value.push({ name: newItemName.value, price: newItemPrice.value });
+    newItemName.value = "";
+    newItemPrice.value = 0;
+  }
+};
+</script>
+
+<template>
+  <div>ItemList</div>
+  <div v-for="item in items" :key="item.name">
+    <div class="item" :class="{ over500: item.price >= 500 }">
+      <div class="name">名前: {{ item.name }}</div>
+      <div class="price">値段: {{ item.price }} 円</div>
+      <div v-if="item.price >= 10000">高額商品</div>
+    </div>
+  </div>
+
+  <div>
+    <label>
+      名前
+      <input v-model="newItemName" type="text" />
+    </label>
+    <label>
+      価格
+      <input v-model="newItemPrice" type="number" />
+    </label>
+    <button @click="addItem">add</button>
+  </div>
+</template>
+
+<style>
+.over500 {
+  color: red;
+}
+</style>
